8ab4048a396e27b03bcc5758b9165b6e260baa8a
Nathat Worakritpong <nathat.wk@gmail.com>
2020-01-25 12:47:51 +0700

    ข้อที่ 8

---
+++ ข้อที่ 8/homework.js
@@ -0,0 +1,20 @@
+function bubbleSort(array) {
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < array.length; i += 1) {
+        if (array[i - 1] > array[i]) {
+          done = false;
+          var tmp = array[i - 1];
+          array[i - 1] = array[i];
+          array[i] = tmp;
+        }
+      }
+    }
+  
+    return array;
+  }
+  
+  var numbers = [0,9,1,8,2,7,3,6,4,5];
+  bubbleSort(numbers);
+  console.log(numbers);
